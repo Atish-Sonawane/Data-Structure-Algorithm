c0ce220b791a291472db5118d5c33e985fc95179
Atish-Sonawane <104720895+Atish-Sonawane@users.noreply.github.com>
2022-05-01 18:06:54 +0530

    Create Geometric progretion.java

---
+++ leetcode/Geometric progretion.java
@@ -0,0 +1,14 @@
+
+public class gp3 {
+
+	public static void main(String[] args) {
+		int kilo=10;
+		int charge = 0; 
+		for(int i=4;i<=kilo;i++) {
+			int a=(i-1)/3;
+		      charge+=a;
+		}
+System.out.println(charge);
+	}
+
+}
