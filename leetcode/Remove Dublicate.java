ed6a3c04c9e2ed7d76669340374023f82e86e0c5
Atish-Sonawane <104720895+Atish-Sonawane@users.noreply.github.com>
2022-05-17 19:07:10 +0530

    Update Remove Dublicate.java

--- leetcode/Remove Dublicate.java
+++ leetcode/Remove Dublicate.java
@@ -1,4 +1,18 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// Time Complexity = O(N)
 
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+    for(int i = 1 ; i<nums.length; i++){
+         if(nums[i]!=nums[i-1]){
+             nums[j++] = nums[i]; 
+         }
+    }
+        return j;
+    }
+}
+        // Second Approch
 
 public static int sum(int [] num) {
 		HashSet<Integer> hs = new HashSet<>();
