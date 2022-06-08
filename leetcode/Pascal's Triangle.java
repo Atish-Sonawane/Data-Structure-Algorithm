0f929ad6e8eb111e505820459033801254386841
Atish-Sonawane <104720895+Atish-Sonawane@users.noreply.github.com>
2022-06-08 11:27:46 +0530

    Create Pascal's Triangle.java

---
+++ leetcode/Pascal's Triangle.java
@@ -0,0 +1,24 @@
+https://leetcode.com/problems/pascals-triangle/
+
+class Solution {
+public  List<List<Integer>> generate(int numRows) {
+         List<List<Integer>> result = new ArrayList<List<Integer>>();
+    	 List<Integer> pre = new ArrayList<Integer>();
+          for(int i = 1 ; i<=numRows ; i++) {
+         	 List<Integer> row = new ArrayList<Integer>();
+
+        	  for(int j=0 ; j<i ; j++) {
+        		  if(j == 0 || j == i-1 ) {
+        			  row.add(1); 
+        		  }
+        		  else {
+        			  row.add(pre.get(j)+pre.get(j-1));
+        		  }
+        		  
+        	  }
+        	  pre = row;
+        	  result.add(row);
+          }
+	return result;
+	 }
+}
