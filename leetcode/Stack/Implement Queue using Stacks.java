edb1024b93cd51f5ca8f86e14e696089f77a541e
Atish-Sonawane <104720895+Atish-Sonawane@users.noreply.github.com>
2022-06-30 11:35:54 +0530

    Create Implement Queue using Stacks.java

---
+++ leetcode/Stack/Implement Queue using Stacks.java
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/implement-queue-using-stacks/
+//
+
+
+class MyQueue {
+
+    public Stack<Integer> stack1 = new Stack<Integer>();
+    public Stack<Integer> stack2 = new Stack<Integer>();
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        while(!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        stack1.push(x);
+        while(!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
+    
+    public int pop() {
+        return stack1.pop();
+    }
+    
+    public int peek() {
+        return stack1.peek();
+    }
+    
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+}
