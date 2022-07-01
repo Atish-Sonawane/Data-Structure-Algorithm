63b3139128ad7f86475558697010c24f45fbb0e7
Atish-Sonawane <104720895+Atish-Sonawane@users.noreply.github.com>
2022-07-01 14:29:19 +0530

    Create Valid Parentheses.java

---
+++ leetcode/Stack/Valid Parentheses.java
@@ -0,0 +1,73 @@
+//https://leetcode.com/problems/valid-parentheses/
+//TLE (Not Working Good)
+
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2==1){
+            return false;
+        }
+        //int check = 0;
+        while(s.length()>0){
+            // check = s.length();
+            s=s.replace("()", "");
+            s=s.replace("[]", "");
+            s=s.replace("{}", "");
+           // if(check == s.length()){
+            if(s.length()==0){
+                return true;
+            }
+        }
+        return false;
+            
+    }
+}
+
+// better approch
+
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2==1){
+            return false;
+        }
+       // int check = 0;
+        while(s.length()>0){
+            int check = s.length();
+            s=s.replace("()", "");
+            s=s.replace("[]", "");
+            s=s.replace("{}", "");
+             if(check == s.length()){
+                return false;
+            }
+        }
+        return true;
+            
+    }
+}
+
+// best Approch using stack
+
+class Solution {
+    public boolean isValid(String s) {
+    Stack<Character> st = new Stack<>();
+
+    for(int i=0;i<s.length();i++){
+        if(st.empty()){
+            st.push(s.charAt(i));
+        }
+       else if(st.peek()=='(' && s.charAt(i)==')'){
+            st.pop();
+        }
+         else if(st.peek()=='[' && s.charAt(i)==']'){
+            st.pop();
+        }
+         else if(st.peek()=='{' && s.charAt(i)=='}'){
+            st.pop();
+        }
+        else{
+            st.push(s.charAt(i));
+        }
+    }
+    return st.empty();
+    
+    }
+}
