9132bc813ea02ac9dece42a967a9bed024fe0a78
Atish-Sonawane <104720895+Atish-Sonawane@users.noreply.github.com>
2022-08-16 11:16:34 +0530

    Update Implement strStr().java

--- Interview Question/Implement strStr().java
+++ Interview Question/Implement strStr().java
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/implement-strstr/submissions/
 //TC O(n)
-
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>FASTEST
 class Solution {
     public int strStr(String haystack, String needle) {
       if(needle.isEmpty())
@@ -12,3 +12,54 @@ ans= haystack.indexOf(needle);
 return ans;
     }
 }
+
+//second Approch
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+      if(needle.isEmpty())
+          return 0;
+        
+        int m = haystack.length();
+        int n = needle.length();
+    for (int i = 0; i <= m-n; i++) {
+            if (haystack.substring(i, i+n).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+// BruteForce Approch
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0){
+            return 0;
+        }
+        
+        int i=0,j=0,index=-1;
+        
+        while(i<haystack.length() && j<needle.length()){
+            if(haystack.charAt(i)!=needle.charAt(j)){
+                i++;
+            }else{
+                index = i;
+                while(i<haystack.length() && j<needle.length() && haystack.charAt(i) == needle.charAt(j)){
+                    i++;
+                    j++;
+                }
+                if(j==needle.length()){
+                    return index;
+                }else{
+                    i = index+1;
+                    j = 0;
+                    index = -1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
