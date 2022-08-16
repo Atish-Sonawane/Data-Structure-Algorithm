7913437d72477bfa98247f49f48c41d3997798dd
Atish-Sonawane <104720895+Atish-Sonawane@users.noreply.github.com>
2022-08-16 22:20:53 +0530

    Update Binary Tree Inorder Traversal.java

--- Interview Question/Binary Tree Inorder Traversal.java
+++ Interview Question/Binary Tree Inorder Traversal.java
@@ -1,2 +1,20 @@
 // https://leetcode.com/problems/binary-tree-inorder-traversal/
 //TC- O(n)
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+    public void helper(TreeNode root, List<Integer> res) {
+        if(root == null){
+            return;
+        }
+        else{
+            helper(root.left, res);
+            res.add(root.val);
+            helper(root.right, res);
+    }
+  }
+}
