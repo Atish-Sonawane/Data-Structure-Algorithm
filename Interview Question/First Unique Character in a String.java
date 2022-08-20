0e12a1b42e71b247d082ac74669e8f8ff9c9f3d9
Atish-Sonawane <104720895+Atish-Sonawane@users.noreply.github.com>
2022-08-21 00:46:24 +0530

    Create First Unique Character in a String.java

---
+++ Interview Question/First Unique Character in a String.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/first-unique-character-in-a-string/
+
+class Solution {
+    public int firstUniqChar(String s) {
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
